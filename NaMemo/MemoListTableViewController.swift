be50103c8b982facdd10649bfbdc808fbc02d835
poohhh <ny2322@gmail.com>
2020-06-24 20:53:33 +0900

    IPad 지원 추가

--- NaMemo/MemoListTableViewController.swift
+++ NaMemo/MemoListTableViewController.swift
@@ -75,7 +75,6 @@ class MemoListTableViewController: UITableViewController {
         return cell
     }
 
-
     
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
